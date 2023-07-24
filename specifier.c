298748efdcec4b8b6a8f22ab8eb417b977be2b9e
ismailchiba <ismailchiba0@gmail.com>
2023-07-24 04:33:42 -0700

    specifier.c file

---
+++ specifier.c
@@ -0,0 +1,138 @@
+#include "main.h"
+
+/**
+ * get_specifier - the function format
+ * @s: the format string
+ *
+ * Return: the number of bytes
+ */
+
+int (*get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t specifier[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"x", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifier[i].specifier)
+	{
+		if (*s == specifier[i].specifier[0])
+		{
+			return (specifier[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: the number of bytes printed
+ */
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag function
+ * @s: the format string
+ * @params: the params struct
+ *
+ * Return: if flag exist
+ */
+
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - finds the modifier func
+ * @s: the format string
+ * @params: the parametre struct
+ *
+ * Return: if modifier valid
+ */
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			i = params->h_modifier = 1;
+			break;
+		case 'l':
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_width - the width of the format string
+ * @s: the format string
+ * @params: parameter struct
+ * @ap: argument pointer
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigt(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
