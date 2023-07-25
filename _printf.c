8781d9913c2d3711b6d771ecac3692204e1ece98
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-25 13:46:02 -0700

    update _printf.c file and correct p++  that was under commments

--- _printf.c
+++ _printf.c
@@ -29,9 +29,9 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
+		while (get_flag(p, &params))
 		{
-			p++; /* next char */
+			p++;
 		}
 		p = get_width(p, &params, ap);
 		p = get_precision(p, &params, ap);
