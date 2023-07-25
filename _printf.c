f4bd141c3ef2b00558a60e47f3c9d1c96d00228a
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-25 09:44:23 -0700

    convert_numbers

--- _printf.c
+++ _printf.c
@@ -1,49 +1,50 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - function that produces output according to a format
+ * @format:  a character string composed of 0 or more directives
+ * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-	int count = 0;/*counter count number of characters printe*/
-	va_list args;
-	char *c, *start;
-	params_t params = PARAMS_INIT;/*init by zero al elements*/
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	va_start(args, format);
+	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (c = (char *)format; *c; c++)
+	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, args);
-		if (*c != '%')
+		init_params(&params, ap);
+		if (*p != '%')
 		{
-			count += _putchar(*c);
+			sum += _putchar(*p);
 			continue;
 		}
-		start = c;
-		c++;
-		while (get_flags(c, &params))/*check flags*/
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
 		{
-			c++;/*next char*/
+			p++; /* next char */
 		}
-		c = get_width(c, &params, args);
-		c = get_precision(c, &params, args);
-		if (get_modifier(c, &params))
-			c++;
-		if (!get_specifier(c, &params))
-			count += printf_from_to(start, c,
-			params.l_modifier || params.h_modifier ? c - 1 : 0);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			count += get_print_func(c, args, &params);
+			sum += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(args);
-	return (count);
+	va_end(ap);
+	return (sum);
 }
+	
